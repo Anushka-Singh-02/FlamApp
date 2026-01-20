cbf601c5476d0c006783d058283398aa1b59fbb6
Anushka-Singh-02 <anushkasingh03.as@gmail.com>
2026-01-21 00:05:48 +0530

    flamapp remove package lock

--- server/server.js
+++ server/server.js
@@ -47,4 +47,4 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+server.listen(process.env.PORT||3000, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
